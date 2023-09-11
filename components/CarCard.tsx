13dc4f79ac98a766e187832867bccf4d48e8dd47
crack-stein <rico47198@live.de>
2023-09-11 12:05:30 +0200

    carShowProperties

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -75,7 +76,13 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
-  )
-}
+  );
+};
 export default CarCard;
